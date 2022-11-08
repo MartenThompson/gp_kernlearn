6a94e4cc13e3e690dd028c74cf11b142cef3b06a
Marten L Thompson <thom7058@umn.edu>
2022-11-08 14:10:55 -0600

    trying to understand what kernel we should learn, esp if learning a mean

--- package/gp_kernlearn/vignettes/gp_datagen_eg.R
+++ package/gp_kernlearn/vignettes/gp_datagen_eg.R
@@ -2,12 +2,13 @@
 # Data comes from Y ~ GP(0, K(X))
 
 
-
-
 setwd('~/Git/gp_kernlearn/')
 source('abc/abc.R')
 source('package/gp_kernlearn/code/basis_orthog_poly.R')
 source('package/gp_kernlearn/code/kernlearn.R')
+
+
+
 
 rbf_kernel <- function(X, alpha.0, alpha.1, phi=0) {
   N <- dim(X)[1]
@@ -22,21 +23,30 @@ rbf_kernel <- function(X, alpha.0, alpha.1, phi=0) {
 }
 
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#### p = 1. GP(0, rbf) ####
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+save_slug <- 'package/gp_kernlearn/vignettes/output/rbf_p1/'
+dir.create(file.path(save_slug))
+#dir.create(file.path(save_slug, 'robjects/', fsep=''))
 N <- 50
 
 set.seed(2022)
 X <- matrix(seq(-5,5,length.out=N), nrow=N, ncol=1)
-K <- rbf_kernel(X, 1, 5)
+K <- rbf_kernel(X, 1, 15)
 Y <- t(mvtnorm::rmvnorm(1, rep(0, N), K))
+saveRDS(data.frame(X=X,Y=Y), paste0(save_slug, 'data.RData'))
+saveRDS(K, paste0(save_slug, 'dataK.RData'))
 
 plot(X, Y)
 image(K)
 
-leg_basis_maker <- make_legendre1D_basis_maker(degree=4)
+leg_basis_maker <- make_legendre1D_basis_maker(degree=5)
 b.X <- leg_basis_maker(X)
 #beta.true <- c(-1,3,1,3)
 data_gen <- data_gen_maker(b.X)
-# TODO: prior hard coded 5
+
+param_prior <- param_prior_maker(basis.dim = 5+1)
 garbage <- data_gen(NA, param_prior()) # just need to run this method once o/w lazy init bites us.
 #Y <- data_gen(NA, list(beta=beta.true)) 
 #plot(X, Y)
@@ -44,17 +54,18 @@ garbage <- data_gen(NA, param_prior()) # just need to run this method once o/w l
 M <- 100
 runtime <- 5
 abc.output <- abc_knn_fixedrt(M, runtime, param_prior, data_gen, Y, distance, n_cores=6, k=1, packages_load=c())
+saveRDS(abc.output, paste0(save_slug, 'abc_output_5min.RData'))
 
 par(mfrow=c(2,3))
-for (i in 1:5) {
+for (i in 1:6) {
  plot(density(abc.output$results[,i]), main=paste0('Beta ', i))
  #lines(c(E.leg[i], E.leg[i]), c(0,1), col='red')
 }
 par(mfrow=c(1,1))
 
 # TODO move to kern
-V <- cov(as.matrix(abc.output$results[,1:5])) # TODO: 5 hard coded
-E <- apply(abc.output$results[,1:5], 2, mean) # TODO
+V <- cov(as.matrix(abc.output$results[,1:6])) # TODO: 5 hard coded
+E <- apply(abc.output$results[,1:6], 2, mean) # TODO
 X.test <- matrix(runif(25, -5, 5), ncol=1)
 kern.pieces <- make_kern(X, X.test, V, leg_basis_maker)
 post.ests <- posterior_test_meanvar(X.test, X, kern.pieces$K.traininv, 
@@ -64,6 +75,61 @@ post.ests <- posterior_test_meanvar(X.test, X, kern.pieces$K.traininv,
 plot(X, Y)
 points(X.test, post.ests$post.test.mean, col='green', pch=16)
 image(kern.pieces$K.train)
+image(kern.pieces$K.star)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#### p = 1. GP(x^3, rbf) ####
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+save_slug <- 'package/gp_kernlearn/vignettes/output/rbf_p1_wmean/'
+dir.create(file.path(save_slug))
+
+N <- 50
+
+set.seed(2022)
+X <- matrix(seq(-5,5,length.out=N), nrow=N, ncol=1)
+K <- rbf_kernel(X, 100, .1)
+Y <- t(mvtnorm::rmvnorm(1, X^3, K))
+#saveRDS(data.frame(X=X,Y=Y), paste0(save_slug, 'data.RData'))
+#saveRDS(K, paste0(save_slug, 'dataK.RData'))
+
+plot(X, Y)
+image(K)
+
+leg_basis_maker <- make_legendre1D_basis_maker(degree=5)
+b.X <- leg_basis_maker(X)
+#beta.true <- c(-1,3,1,3)
+data_gen <- data_gen_maker(b.X)
+
+param_prior <- param_prior_maker(basis.dim = 5+1)
+garbage <- data_gen(NA, param_prior()) # just need to run this method once o/w lazy init bites us.
+#Y <- data_gen(NA, list(beta=beta.true)) 
+#plot(X, Y)
+
+M <- 100
+runtime <- 5
+abc.output <- abc_knn_fixedrt(M, runtime, param_prior, data_gen, Y, distance, n_cores=6, k=1, packages_load=c())
+saveRDS(abc.output, paste0(save_slug, 'abc_output_5min.RData'))
+
+par(mfrow=c(2,3))
+for (i in 1:6) {
+  plot(density(abc.output$results[,i]), main=paste0('Beta ', i))
+  #lines(c(E.leg[i], E.leg[i]), c(0,1), col='red')
+}
+par(mfrow=c(1,1))
+
+# TODO move to kern
+V <- cov(as.matrix(abc.output$results[,1:6])) # TODO: 5 hard coded
+E <- apply(abc.output$results[,1:6], 2, mean) # TODO
+X.test <- matrix(runif(25, -5, 5), ncol=1)
+kern.pieces <- make_kern(X, X.test, V, leg_basis_maker)
+post.ests <- posterior_test_meanvar(X.test, X, kern.pieces$K.traininv, 
+                                    kern.pieces$K.startr, kern.pieces$K.star,
+                                    E, Y, leg_basis_maker)
+
+plot(X, Y)
+points(X.test, post.ests$post.test.mean, col='green', pch=16)
+image(kern.pieces$K.train)
+image(kern.pieces$K.star)
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
