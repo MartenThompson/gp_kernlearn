c9ed74040b9bc07102449c5334a69e9b12c64668
Marten L Thompson <thom7058@umn.edu>
2023-02-07 17:58:12 -0600

    full run ready, if brittle

--- package/gp_kernlearn/code/gof_bs_analysis.R
+++ package/gp_kernlearn/code/gof_bs_analysis.R
@@ -3,10 +3,10 @@ rm(list=ls())
 setwd('~/Git/gp_kernlearn/')
 source('package/gp_kernlearn/code/basis_orthog_poly.R')
 source('package/gp_kernlearn/code/kernlearn.R')
-source('package/gp_kernlearn/code/gof_bootstrap.R')
+source('package/gp_kernlearn/code/bootstrap.R')
 
 
-save_slug <- 'package/gp_kernlearn/code/output/gof_bs_frob/'
+save_slug <- 'package/gp_kernlearn/code/output/gof_bs/'
 dir.create(file.path(save_slug))
 dir.create(file.path(paste0(save_slug, '/models')))
 
@@ -17,11 +17,11 @@ set.seed(2023)
 N.x <- 100         # number of x locations
 X <- matrix(seq(-5,5,length.out=N.x), nrow=N.x, ncol=1)
 #K <- lin_kernel(X, 1, 1/25, 0.1)
-#K <- quad_kernel(X, 1, 1/25, 1/25, 0.1)
+K <- quad_kernel(X, 1, 1/25, 1/25, 0.1)
 #K <- cubic_kernel(X, 1, 1/25, 1/25, 1/25, 0.1)
-K <- rbf_kernel(X, 5, 10)
-gen.name <- 'rbf5-10'
-n.train.datasets <- 20
+#K <- rbf_kernel(X, 5, 10)
+gen.name <- 'cubic'#'rbf5-10'
+n.train.datasets <- 100 # this actually needs to be 1e3 before cov(Y.train) looks like K
 
 Y.train <- list()
 for (r in 1:n.train.datasets) {
@@ -36,44 +36,75 @@ plot(Y)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #### Fit/Load Model ####
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-saved.model.path <- paste0(save_slug, 'models/model_Ycubic_tr20_legd3.RData')
+model.name <-  'model_Ycubic_tr100_legd3'
+saved.model.path <- paste0(save_slug, 'models/', model.name, '.RData')
 
 # Either fit a model or load  a saved one.
-if (is.na(saved.model.path)) {
-  deg <- 3
+if (is.na(model.name)) {
+  deg <- 4
   leg_basis_maker <- make_legendre1D_basis_maker(degree = deg)
   b.X <- leg_basis_maker(X)
   model <- fit_kernlearn(b.X, leg_basis_maker, Y.train)
-  saveRDS(model, paste0(save_slug, 'models/model_', 'Y', gen.name, '_tr', n.train.datasets, '_legd', deg ,'.RData'))
+  model$K.true <- K
+  saveRDS(model, paste0(save_slug, 'models/model_', 'Y', gen.name, 
+                        '_tr', n.train.datasets, '_legd', deg ,'.RData'))
 } else {
   model <- readRDS(saved.model.path)  
   
-  model_data_gen <- function() {
+  model_data_gen_single <- function() {
     K.est <- model$K.est
     return(matrix(mvtnorm::rmvnorm(1,sigma=K.est), ncol=1))
   }
 }
 
 
-plot(model_data_gen())
+plot(model_data_gen_single())
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #### Bootstrapping ####
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-frob.norm <- function(D) {
-  return(norm(D, type='F'))
+model_data_gen <- function() {
+  # matrix with n.train.datasets rows, each a synthetic obs from our model
+  t(sapply(1:n.train.datasets, function(x){model_data_gen_single()}))
 }
 
-training.sumstats <- unlist(lapply(Y.train, frob.norm))
-n.boot <- 1e3
-boot.sumstats <- bootstrap_stat_dist(frob.norm, model_data_gen, n.boot)
+frob_norm_cov <- function(mat) {
+  return(norm(cov(mat), type='F'))
+}
+
+one_norm_cov <- function(mat) {
+  return(norm(cov(mat), type='O'))
+}
+
+infinity_norm_cov <- function(mat) {
+  return(norm(cov(mat), type='I'))
+}
+
+spectral_norm_cov <- function(mat) {
+  return(norm(cov(mat), type='2'))
+}
+
+obs.mat <- matrix(unlist(model$Y.train), ncol=N.x, byrow = TRUE)
+obs.cov.mat <- cov(obs.mat)
+image(obs.cov.mat)
+image(model$K.true)
+image(model$K.est)
 
 
-plot(density(boot.sumstats), xlim=c(0, max(boot.sumstats, training.sumstats)))
-points(training.sumstats, rep(0, length(training.sumstats)), col='red', pch=16)
+norm.name <- 'frob_cov/'    # UPDATE 1/3
+training.sumstat <- frob_norm_cov(obs.mat) # UPDATE 2/3
+n.boot <- 1e2
+boot.sumstats <- bootstrap_stat_dist(infinity_norm_cov, model_data_gen, n.boot) # UPDATE 3/3
 
-bs.savepath <- paste0(strsplit(saved.model.path, '.', fixed=T)[[1]][1], '_bs.RData')
-saveRDS(list(training.sumstats=training.sumstats,
+bs.output.path <- saved.model.path <- paste0(save_slug, norm.name, model.name)
+
+png(paste0(bs.output.path, '_bs', n.boot,'dens.png'), height=6, width=6, units='in', res = 100)
+plot(density(boot.sumstats), xlim=c(0, max(boot.sumstats, training.sumstat)))
+points(training.sumstat, rep(0, length(training.sumstat)), col='red', pch=16)
+dev.off()
+
+bs.savepath <- paste0(bs.output.path, '_bs', n.boot,'.RData')
+saveRDS(list(training.sumstat=training.sumstat,
              boot.sumstats=boot.sumstats),
         bs.savepath)
 
