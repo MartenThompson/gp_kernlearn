7dccfea79a694b1a0aca9cd704715142ab9284d0
Marten Thompson <martenthompson@gmail.com>
2023-02-07 21:05:08 -0600

    param tweaks for analysis

--- package/gp_kernlearn/code/gof_bs_analysis.R
+++ package/gp_kernlearn/code/gof_bs_analysis.R
@@ -3,8 +3,6 @@ rm(list=ls())
 setwd('~/Git/gp_kernlearn/')
 source('package/gp_kernlearn/code/basis_orthog_poly.R')
 source('package/gp_kernlearn/code/kernlearn.R')
-source('package/gp_kernlearn/code/bootstrap.R')
-
 
 save_slug <- 'package/gp_kernlearn/code/output/gof_bs/'
 dir.create(file.path(save_slug))
@@ -17,10 +15,10 @@ set.seed(2023)
 N.x <- 100         # number of x locations
 X <- matrix(seq(-5,5,length.out=N.x), nrow=N.x, ncol=1)
 #K <- lin_kernel(X, 1, 1/25, 0.1)
-K <- quad_kernel(X, 1, 1/25, 1/25, 0.1)
+#K <- quad_kernel(X, 1, 1/25, 1/25, 0.1)
 #K <- cubic_kernel(X, 1, 1/25, 1/25, 1/25, 0.1)
-#K <- rbf_kernel(X, 5, 10)
-gen.name <- 'cubic'#'rbf5-10'
+K <- rbf_kernel(X, 5, 10)
+gen.name <- 'rbf5-10'
 n.train.datasets <- 100 # this actually needs to be 1e3 before cov(Y.train) looks like K
 
 Y.train <- list()
@@ -36,16 +34,18 @@ plot(Y)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #### Fit/Load Model ####
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-model.name <-  'model_Ycubic_tr100_legd3'
+model.name <- NA #'model_Ycubic_tr100_legd4'
 saved.model.path <- paste0(save_slug, 'models/', model.name, '.RData')
 
 # Either fit a model or load  a saved one.
 if (is.na(model.name)) {
-  deg <- 4
+  deg <- 3
   leg_basis_maker <- make_legendre1D_basis_maker(degree = deg)
   b.X <- leg_basis_maker(X)
   model <- fit_kernlearn(b.X, leg_basis_maker, Y.train)
   model$K.true <- K
+  model$N.x <- N.x
+  model$n.train.datasets <- n.train.datasets
   saveRDS(model, paste0(save_slug, 'models/model_', 'Y', gen.name, 
                         '_tr', n.train.datasets, '_legd', deg ,'.RData'))
 } else {
@@ -63,38 +63,25 @@ plot(model_data_gen_single())
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #### Bootstrapping ####
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+source('package/gp_kernlearn/code/bootstrap.R')
+source('package/gp_kernlearn/code/norms.R')
+
 model_data_gen <- function() {
   # matrix with n.train.datasets rows, each a synthetic obs from our model
-  t(sapply(1:n.train.datasets, function(x){model_data_gen_single()}))
+  t(sapply(1:model$n.train.datasets, function(x){model_data_gen_single()}))
 }
 
-frob_norm_cov <- function(mat) {
-  return(norm(cov(mat), type='F'))
-}
-
-one_norm_cov <- function(mat) {
-  return(norm(cov(mat), type='O'))
-}
-
-infinity_norm_cov <- function(mat) {
-  return(norm(cov(mat), type='I'))
-}
-
-spectral_norm_cov <- function(mat) {
-  return(norm(cov(mat), type='2'))
-}
-
-obs.mat <- matrix(unlist(model$Y.train), ncol=N.x, byrow = TRUE)
+obs.mat <- matrix(unlist(model$Y.train), ncol=model$N.x, byrow = TRUE)
 obs.cov.mat <- cov(obs.mat)
 image(obs.cov.mat)
 image(model$K.true)
 image(model$K.est)
 
-
-norm.name <- 'frob_cov/'    # UPDATE 1/3
-training.sumstat <- frob_norm_cov(obs.mat) # UPDATE 2/3
-n.boot <- 1e2
-boot.sumstats <- bootstrap_stat_dist(infinity_norm_cov, model_data_gen, n.boot) # UPDATE 3/3
+set.seed(2023)
+norm.name <- 'spectral_cov/'    # UPDATE 1/3
+training.sumstat <- spectral_norm_cov(obs.mat) # UPDATE 2/3
+n.boot <- 2e2
+boot.sumstats <- bootstrap_stat_dist(spectral_norm_cov, model_data_gen, n.boot) # UPDATE 3/3
 
 bs.output.path <- saved.model.path <- paste0(save_slug, norm.name, model.name)
 
