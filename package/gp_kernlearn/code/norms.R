86c055123a470ebcd0b3695c0ddded710bed36df
Marten L Thompson <thom7058@umn.edu>
2023-02-08 10:36:04 -0600

    descriptions for norms

--- package/gp_kernlearn/code/norms.R
+++ package/gp_kernlearn/code/norms.R
@@ -1,17 +1,23 @@
 
 
+# aka Euclidean norm, sqrt of sum of |a_ij|^2
 frob_norm_cov <- function(mat) {
   return(norm(cov(mat), type='F'))
 }
 
+
+# Larges col sum (of abs values)
 one_norm_cov <- function(mat) {
   return(norm(cov(mat), type='O'))
 }
 
+
+# Largest row sum (of abs values)
 infinity_norm_cov <- function(mat) {
   return(norm(cov(mat), type='I'))
 }
 
+# sqrt of max eigval of A^H A
 spectral_norm_cov <- function(mat) {
   return(norm(cov(mat), type='2'))
 }
