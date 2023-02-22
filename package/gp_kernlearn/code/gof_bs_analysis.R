4281204f4bf401ea696bbe76ede99f49db4e61f8
Marten Thompson <martenthompson@gmail.com>
2023-02-21 18:41:34 -0600

    trying to understand if eigenvalues can help us choose J

--- package/gp_kernlearn/code/gof_bs_analysis.R
+++ package/gp_kernlearn/code/gof_bs_analysis.R
@@ -13,12 +13,12 @@ dir.create(file.path(paste0(save_slug, 'models')))
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 set.seed(2023)
 N.x <- 100         # number of x locations
-X <- matrix(seq(-5,5,length.out=N.x), nrow=N.x, ncol=1)
+X <- matrix(seq(-1,1,length.out=N.x), nrow=N.x, ncol=1)
 #K <- lin_kernel(X, 1, 1/25, 0.1)
 #K <- quad_kernel(X, 1, 1/25, 1/25, 0.1)
-#K <- cubic_kernel(X, 1, 1/25, 1/25, 1/25, 0.1)
-K <- rbf_kernel(X, 5, 10)
-gen.name <- 'rbf5-10'
+#K <- cubic_kernel(X, 1, 1, 1, 1, 0.1)
+K <- rbf_kernel(X, 5, 0.05, 0.1)
+gen.name <- 'rbf5-005-fz01'
 n.train.datasets <- 100 # this actually needs to be 1e3 before cov(Y.train) looks like K
 
 Y.train <- list()
@@ -34,12 +34,12 @@ plot(Y)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #### Fit/Load Model ####
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-model.name <- 'model_Yrbf5-10_tr100_legd5'
+model.name <- 'model_Yrbf5-005-fz01_tr100_legd10'
 saved.model.path <- paste0(save_slug, 'models/', model.name, '.RData')
 
 # Either fit a model or load  a saved one.
 if (is.na(model.name)) {
-  deg <- 5
+  deg <- 10
   leg_basis_maker <- make_legendre1D_basis_maker(degree = deg)
   b.X <- leg_basis_maker(X)
   model <- fit_kernlearn(b.X, leg_basis_maker, Y.train)
@@ -58,7 +58,7 @@ if (is.na(model.name)) {
 }
 
 
-#plot(model_data_gen_single())
+plot(model_data_gen_single())
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #### Bootstrapping ####
@@ -73,31 +73,37 @@ model_data_gen <- function() {
 
 obs.mat <- matrix(unlist(model$Y.train), ncol=model$N.x, byrow = TRUE)
 obs.cov.mat <- cov(obs.mat)
-image(obs.cov.mat)
-image(model$K.true)
-image(model$K.est)
-image(model$V.est)
+#image(obs.cov.mat)
+#image(model$K.true)
+#image(model$K.est)
+#image(model$V.est)
 
 set.seed(2023)
 norm.name <- 'eigenvals_cov/'    # UPDATE 1/3
 training.sumstat <- eigenvals_cov(obs.mat) # UPDATE 2/3
 n.boot <- 2e2
 boot.sumstats <- bootstrap_stat_dist(eigenvals_cov, model_data_gen, n.boot) # UPDATE 3/3
-
 bs.output.path <- paste0(save_slug, norm.name, model.name)
 
 #png(paste0(bs.output.path, '_bs', n.boot,'dens.png'), height=6, width=6, units='in', res = 100)
-stat.dim <- 50
-plot(density(boot.sumstats[,stat.dim]), xlim=c(0, max(boot.sumstats[,stat.dim], training.sumstat[stat.dim])))
-points(training.sumstat[stat.dim], c(0), col='red', pch=16)
+#stat.dim <- 1
+#plot(density(boot.sumstats[,stat.dim]), xlim=c(0, max(boot.sumstats[,stat.dim], training.sumstat[stat.dim])))
+#points(training.sumstat[stat.dim], c(0), col='red', pch=16)
 #dev.off()
 
 
-plot(log(ts(eigen(model$K.true)$values[1:stat.dim])), col='green', 
+stat.dim <- 40
+png(paste0(bs.output.path, '_bs', n.boot, '_eigenmatch', stat.dim, '.png'), height=6, width=6, units='in', res=100)
+plot(log(ts(eigen(model$K.true)$values[1:stat.dim])), col='green',
      ylim=log(c(min(training.sumstat[1:stat.dim], apply(boot.sumstats[,1:stat.dim], 2, mean)),
-            max(training.sumstat[1:stat.dim], apply(boot.sumstats[,1:stat.dim], 2, mean)))))
-lines(log(ts(training.sumstat[1:stat.dim])), col='blue')
-lines(log(ts(apply(boot.sumstats[,1:stat.dim], 2, mean))), col='magenta')
+            max(training.sumstat[1:stat.dim], apply(boot.sumstats[,1:stat.dim], 2, mean)))),
+     lwd=2,
+     xlab='Eigenvalue Order', ylab='log(Eigenvalue)', 
+     main=model.name)
+lines(log(ts(training.sumstat[1:stat.dim])), col='blue', lwd=2)
+lines(log(ts(apply(boot.sumstats[,1:stat.dim], 2, mean))), col='magenta', lwd=2)
+legend(25, 4, c('True', 'Obs', 'Mean Boot'), c('green', 'blue', 'magenta'))
+dev.off()
 
 bs.savepath <- paste0(bs.output.path, '_bs', n.boot,'.RData')
 saveRDS(list(training.sumstat=training.sumstat,
