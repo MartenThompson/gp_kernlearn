6dc7cfd984bd849e6499b96731fa6c33f65872ba
Marten L Thompson <thom7058@umn.edu>
2023-03-17 17:07:09 -0500

    better figure comparing stationary methods

--- package/gp_kernlearn/code/stationary_analysis.R
+++ package/gp_kernlearn/code/stationary_analysis.R
@@ -5,9 +5,12 @@ setwd('~/Git/gp_kernlearn/')
 #source('package/gp_kernlearn/code/kernlearn.R')
 source('package/gp_kernlearn/code/stationary.R')
 
-#save_slug <- 'package/gp_kernlearn/code/output/stat/'
-#dir.create(file.path(save_slug))
+get_unique <- function(D, K) {
+  
+}
 
+save_slug <- 'package/gp_kernlearn/code/output/stat/'
+dir.create(file.path(save_slug))
 
 model.name <- 'model_Yrbf5-05-fz01_tr100_legd10'
 saved.model.path <- paste0('package/gp_kernlearn/code/output/gof_bs/models/', model.name, '.RData')
@@ -17,17 +20,19 @@ X <- model$b.X.train[,2] # TODO it would be cool if kernlearn just saved X untou
 
 #s.x <- s.out.lm$D
 #s.y <- s.out.lm$modtrain
-s.out.lm <- make_stationary(model$K.est, X, dist_euc, modfit_lm)
-s.out.p3 <- make_stationary(model$K.est, X, dist_euc, modfit_lm3)
-s.out.mon3 <- make_stationary(model$K.est, X, dist_euc, modfit_monopoly_maker(3))
+K.corr <- model$K.est/max(model$K.est)
+s.out.lm <- make_stationary(K.corr, X, dist_euc, modfit_lm)
+s.out.p3 <- make_stationary(K.corr, X, dist_euc, modfit_lm3)
+s.out.mon3 <- make_stationary(K.corr, X, dist_euc, modfit_monopoly_maker(3))
 
-plot(s.out.lm$D, s.out.lm$modtrain, col='grey')
-points(s.out.lm$D, model$K.true[upper.tri(model$K.true, diag=TRUE)], pch=16, col='green')
-points(s.out.lm$D, s.out.lm$modpred, pch=16, col='red')
-points(s.out.p3$D, s.out.p3$modpred, pch=16, col='orange')
-points(s.out.mon3$D, s.out.mon3$modpred, pch=16, col='magenta')
+plot(s.out.lm$D, s.out.lm$modtrain, col='black', cex=0.1, ylab='Correlation', xlab='Distance')
+lines(s.out.lm$D[4951:5050], model$K.true[100,]/max(model$K.true), pch=16, col='green', lwd=3)
+lines(s.out.lm$D[4951:5050], s.out.lm$K.stationary[100,], col='red', lwd=3)
+lines(s.out.p3$D[4951:5050], s.out.p3$K.stationary[100,], col='orange', lwd=3)
+lines(s.out.mon3$D[4951:5050], s.out.mon3$K.stationary[100,], col='magenta', lwd=3)
 legend('topright', c('Dist(K hat)', 'True', 'Linear', 'Poly 3', 'Mono 3'), 
-       col=c('grey', 'green', 'red', 'orange', 'magenta'), pch = 16)
+       col=c('black', 'green', 'red', 'orange', 'magenta'), pch=c(16, rep(NA, 4)), lwd = c(NA, rep(3, 4)))
+
 
 
 model$K.true[1:3]
