893692e95b2a881dc104c288a0f23517b61191ac
Marten Thompson <martenthompson@gmail.com>
2023-03-27 08:33:54 -0500

    further dev

--- package/gp_kernlearn/code/gof_bs_analysis.R
+++ package/gp_kernlearn/code/gof_bs_analysis.R
@@ -3,6 +3,7 @@ rm(list=ls())
 setwd('~/Git/gp_kernlearn/')
 source('package/gp_kernlearn/code/basis_orthog_poly.R')
 source('package/gp_kernlearn/code/kernlearn.R')
+source('package/gp_kernlearn/code/stationary.R')
 
 save_slug <- 'package/gp_kernlearn/code/output/gof_bs/'
 dir.create(file.path(save_slug))
@@ -15,10 +16,10 @@ set.seed(2023)
 N.x <- 100         # number of x locations
 X <- matrix(seq(-1,1,length.out=N.x), nrow=N.x, ncol=1)
 #K <- lin_kernel(X, 1, 1, 0.1)
-K <- quad_kernel(X, 1, 1, 1, 0.1)
+#K <- quad_kernel(X, 1, 1, 1, 0.1)
 #K <- cubic_kernel(X, 1, 1, 1, 1, 0.1)
-#K <- rbf_kernel(X, 5, 0.5, 0.1) 
-gen.name <- 'quad1-1-1' # 'rbf5-05-fz01'
+K <- rbf_kernel(X, 5, 0.05, 0.1) 
+gen.name <- 'rbf5-005-fz01' # 'lin1-1' 
 n.train.datasets <- 100 # this actually needs to be 1e3 before cov(Y.train) looks like K
 
 Y.train <- list()
@@ -34,12 +35,12 @@ plot(Y)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #### Fit/Load Model ####
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-model.name <- NA #'model_Yrbf5-05-fz01_tr100_legd10'
+model.name <- 'model_Ycubic_tr100_legd3' # NA
 saved.model.path <- paste0(save_slug, 'models/', model.name, '.RData')
 
 # Either fit a model or load  a saved one.
 if (is.na(model.name)) {
-  deg <- 10
+  deg <- 2
   leg_basis_maker <- make_legendre1D_basis_maker(degree = deg)
   b.X <- leg_basis_maker(X)
   model <- fit_kernlearn(b.X, leg_basis_maker, Y.train)
@@ -54,6 +55,11 @@ if (is.na(model.name)) {
   model_data_gen_single <- function() {
     K.est <- model$K.est
     return(matrix(mvtnorm::rmvnorm(1,sigma=K.est), ncol=1))
+  }
+  X <- model$b.X.train[,2]
+  s.out.mon3 <- make_stationary(model$K.est, X, dist_euc, modfit_monopoly_maker(3))
+  model_data_gen_stationary_single <- function() {
+    return(matrix(mvtnorm::rmvnorm(1,sigma=s.out.mon3$K.stationary), ncol=1))
   }
 }
 
@@ -70,50 +76,61 @@ model_data_gen <- function() {
   # matrix with n.train.datasets rows, each a synthetic obs from our model
   t(sapply(1:model$n.train.datasets, function(x){model_data_gen_single()}))
 }
+#model_data_gen_stationary <- function() {
+  # matrix with n.train.datasets rows, each a synthetic obs from our model
+#  t(sapply(1:model$n.train.datasets, function(x){model_data_gen_stationary_single()}))
+#}
 
 obs.mat <- matrix(unlist(model$Y.train), ncol=model$N.x, byrow = TRUE)
-obs.cov.mat <- cov(obs.mat)
+#obs.cov.mat <- cov(obs.mat)
 #image(obs.cov.mat)
 #image(model$K.true)
 #image(model$K.est)
 #image(model$V.est)
 
 set.seed(2023)
-norm.name <- 'eigenvals_cov/'    # UPDATE 1/3
-training.sumstat <- eigenvals_cov(obs.mat) # UPDATE 2/3
+norm.name <- 'wem_uniform/'    # UPDATE 1/3
+K = 5
+norm <- weighted_eval_cov_maker(K=K, schedule = rep(1,K))
+training.sumstat <- norm(obs.mat) # UPDATE 2/3
 n.boot <- 2e2
-boot.sumstats <- bootstrap_stat_dist(eigenvals_cov, model_data_gen, n.boot) # UPDATE 3/3
-bs.output.path <- paste0(save_slug, norm.name, model.name)
+boot.sumstats <- bootstrap_stat_dist(norm, model_data_gen, n.boot) # UPDATE 3/3
+bs.output.path <- paste0(save_slug, norm.name, model.name, '_K', K)
 
-#png(paste0(bs.output.path, '_bs', n.boot,'dens.png'), height=6, width=6, units='in', res = 100)
-#stat.dim <- 1
-#plot(density(boot.sumstats[,stat.dim]), xlim=c(0, max(boot.sumstats[,stat.dim], training.sumstat[stat.dim])))
-#points(training.sumstat[stat.dim], c(0), col='red', pch=16)
-#dev.off()
+png(paste0(bs.output.path, '_bs', n.boot,'dens.png'), height=4, width=4, units='in', res = 100)
+stat.dim <- 1
+plot(density(boot.sumstats[,stat.dim]), xlim=c(0, max(boot.sumstats[,stat.dim], training.sumstat[stat.dim])),
+     xlab=NA, main=NA)
+points(training.sumstat[stat.dim], c(0), col='red', pch=16)
+dev.off()
+
+bs.savepath <- paste0(bs.output.path, '_bs', n.boot,'.RData')
+saveRDS(list(training.sumstat=training.sumstat,
+             K = K, norm=norm,
+             boot.sumstats=boot.sumstats),
+        bs.savepath)
+
 
 #dat.in <- readRDS('package/gp_kernlearn/code/output/gof_bs/eigenvals_cov/model_Ycubic_tr100_legd1_bs200.RData')
 #training.sumstat <- dat.in$training.sumstat
 #boot.sumstats <- dat.in$boot.sumstats
 
-stat.dim <- 40
-png(paste0(bs.output.path, '_bs', n.boot, '_eigenmatch', stat.dim, '.png'), height=6, width=6, units='in', res=100)
-plot(log(ts(eigen(model$K.true)$values[1:stat.dim])), col='green',
-     ylim=log(c(min(training.sumstat[1:stat.dim], apply(boot.sumstats[,1:stat.dim], 2, mean)),
-            max(training.sumstat[1:stat.dim], apply(boot.sumstats[,1:stat.dim], 2, mean)))),
-     lwd=2,
-     xlab='Eigenvalue Order', ylab='log(Eigenvalue)', 
-     main=model.name)
-lines(log(ts(training.sumstat[1:stat.dim])), col='blue', lwd=2)
-boot.means <- apply(boot.sumstats[,1:stat.dim], 2, mean)
-boot.sds <- apply(boot.sumstats[,1:stat.dim], 2, sd)
-lines(log(ts(boot.means)), col='magenta', lwd=2)
-polygon(c(1:stat.dim, rev(1:stat.dim)), c(log(boot.means + 2*boot.sds), rev(log(boot.means-2*boot.sds))),
-        col=rgb(1,0,1,0.25), border=NA)
-legend('topright', c('True', 'Obs', 'Mean Boot'), col=c('green', 'blue', 'magenta'), lty=1, lwd=3)
-dev.off()
+# stat.dim <- 40
+# png(paste0(bs.output.path, '_bs', n.boot, '_eigenmatch', stat.dim, '.png'), height=6, width=6, units='in', res=100)
+# plot(log(ts(eigen(model$K.true)$values[1:stat.dim])), col='green',
+#      ylim=log(c(min(training.sumstat[1:stat.dim], apply(boot.sumstats[,1:stat.dim], 2, mean)),
+#             max(training.sumstat[1:stat.dim], apply(boot.sumstats[,1:stat.dim], 2, mean)))),
+#      lwd=2,
+#      xlab='Eigenvalue Order', ylab='log(Eigenvalue)', 
+#      main=model.name)
+# lines(log(ts(training.sumstat[1:stat.dim])), col='blue', lwd=2)
+# boot.means <- apply(boot.sumstats[,1:stat.dim], 2, mean)
+# boot.sds <- apply(boot.sumstats[,1:stat.dim], 2, sd)
+# lines(log(ts(boot.means)), col='magenta', lwd=2)
+# polygon(c(1:stat.dim, rev(1:stat.dim)), c(log(boot.means + 2*boot.sds), rev(log(boot.means-2*boot.sds))),
+#         col=rgb(1,0,1,0.25), border=NA)
+# legend('topright', c('True', 'Obs', 'Mean Boot'), col=c('green', 'blue', 'magenta'), lty=1, lwd=3)
+# dev.off()
 
-bs.savepath <- paste0(bs.output.path, '_bs', n.boot,'.RData')
-saveRDS(list(training.sumstat=training.sumstat,
-             boot.sumstats=boot.sumstats),
-        bs.savepath)
-
+# TODO: bootstrap inconsistent for model selection. Parametric bs is (and is it equiv to AIC, BIC depending on resample size). 
+# Also, dependent data.
--- package/gp_kernlearn/code/model_choice_analysis.R
+++ package/gp_kernlearn/code/model_choice_analysis.R
@@ -56,6 +56,8 @@ output$facet_name <- factor(output$data_gen,
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #### Get AIC/BIC for Our Models (stationary) ####
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+rm(list=ls())
+source('./package/gp_kernlearn/code/model_choice_ic.R')
 source('package/gp_kernlearn/code/stationary.R')
 
 models <- list.files('./package/gp_kernlearn/code/output/gof_bs/models/')
@@ -94,14 +96,40 @@ for (i in 1:nrow(output.stationary)) {
   output.stationary$bic_stationary[i] <- bic(llik, leg.deg, n)
 }
 
-write.csv(output.stationary, paste0('./package/gp_kernlearn/code/output/modchoice_ic/', 'output_stationary_mon3.csv'), row.names = FALSE)
+#write.csv(output.stationary, paste0('./package/gp_kernlearn/code/output/modchoice_ic/', 'output_stationary_mon3.csv'), row.names = FALSE)
 
 output.stationary <- read.csv(paste0('./package/gp_kernlearn/code/output/modchoice_ic/', 'output_stationary_mon3.csv'))
-
 output.stationary <- output.stationary[output.stationary$data_gen != 'Ylin',]
+#output.stationary <- output.stationary[is.finite(output.stationary$aic_stationary), ]
+
+summary(output.stationary)
 output.stationary$facet_name <- factor(output.stationary$data_gen, 
-                            levels=c('Ylin1-1', 'Ycubic', 'Yquad1-1-1', 
+                            levels=c('Ylin1-1', 'Yquad1-1-1', 'Ycubic', 
                                      'Yrbf5-10', 'Yrbf5-05-fz01','Yrbf5-005-fz01'))
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#### Yrbf5-005-fz01 leg 1 ####
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# rm(list=ls())
+# source('./package/gp_kernlearn/code/model_choice_ic.R')
+# source('package/gp_kernlearn/code/stationary.R')
+# 
+# 
+# dataset.reps <- 100
+# model <- readRDS(paste0('./package/gp_kernlearn/code/output/gof_bs/models/', 'model_Yrbf5-005-fz01_tr100_legd1.RData'))
+# 
+# dataset.itr <- 1 # ifelse(0==i%%dataset.reps, dataset.reps, i%%dataset.reps)
+# n <- length(model$Y.train[[dataset.itr]])
+# 
+# X <- model$b.X.train[,2]
+# s.out.mon3 <- make_stationary(model$K.est, X, dist_euc, modfit_monopoly_maker(3))
+# llik <- mvn_loglikelihood(model$Y.train[[dataset.itr]][,1], mu = rep(0, n), s.out.mon3$K.stationary)
+# output.stationary$llik_stationary[i] <- llik
+# output.stationary$aic_stationary[i] <- aic(llik, leg.deg)
+# output.stationary$bic_stationary[i] <- bic(llik, leg.deg, n)
+# It just has a likelihood of 0
+
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #### Fit Basic GP ####
@@ -109,14 +137,25 @@ output.stationary$facet_name <- factor(output.stationary$data_gen,
 library(gplite)
 
 models <- list.files('./package/gp_kernlearn/code/output/gof_bs/models/')
+all.models <- data.frame(
+  full = models,
+  short = rep(NA, length(models))
+)
+for (i in 1:length(models)) {
+  name <- strsplit(models[i], '_')[[1]][2]
+  all.models$short[i] <- name
+}
+
+unique.datagen <- all.models[match(unique(all.models$short), all.models$short),]
+
 dataset.reps <- 100
 
 output.gp <- data.frame(
-  model=models,
-  data_gen=rep(NA, length(models)),
-  llik=rep(NA, length(models)),
-  aic=rep(NA, length(models)),
-  bic=rep(NA, length(models))
+  model=unique.datagen$full,
+  data_gen=rep(NA, nrow(unique.datagen)),
+  llik=rep(NA, nrow(unique.datagen)),
+  aic=rep(NA, nrow(unique.datagen)),
+  bic=rep(NA, nrow(unique.datagen))
 )
 
 for (i in 1:nrow(output.gp)) {
@@ -155,25 +194,44 @@ for (i in 1:nrow(output.gp)) {
   output.gp$llik[i] <- llik
   output.gp$aic[i] <- aic(llik, rbf.df)
   output.gp$bic[i] <- bic(llik, rbf.df, n)
-  break
 }
 
-output.gp <- output.gp[!is.na(output.gp$llik),]
+#output.gp <- output.gp[!is.na(output.gp$llik),]
 output.gp
+write.csv(output.gp, paste0('./package/gp_kernlearn/code/output/modchoice_ic/', 'output_gp_rbf.csv'), row.names = FALSE)
+
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #### Plotting ####
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+integer_breaks <- function(n = 5, ...) {
+  fxn <- function(x) {
+    breaks <- floor(pretty(x, n, ...))
+    names(breaks) <- attr(breaks, "labels")
+    breaks
+  }
+  return(fxn)
+}
+
 library(ggplot2)
-ggplot(data=output.stationary) + 
-  geom_line(aes(x=leg_deg, y=aic_stationary, group=dataset_num, color=data_gen), alpha=0.5) +
+png(paste0('./package/gp_kernlearn/code/output/modchoice_ic/', 'aic.png'), height=5, width=7, units='in', res = 100)
+ggplot(data=output) + 
+  geom_line(aes(x=leg_deg, y=aic, group=dataset_num, color=data_gen), alpha=0.5) +
+  scale_x_continuous(breaks=integer_breaks()) + 
   facet_wrap(~facet_name, scale='free') +
-  theme(legend.position = 'bottom') #+
-#  scale_y_continuous(trans='log10')
+  ylab('AIC') + xlab('degree') + 
+  theme_bw() + 
+  theme(legend.position = 'none') #+
+dev.off()
 
 
-ggplot(data=output.stationary) + 
-  geom_line(aes(x=leg_deg, y=bic_stationary, group=dataset_num, color=data_gen), alpha=0.5) +
+png(paste0('./package/gp_kernlearn/code/output/modchoice_ic/', 'bic.png'), height=5, width=7, units='in', res = 100)
+ggplot(data=output) + 
+  geom_line(aes(x=leg_deg, y=bic, group=dataset_num, color=data_gen), alpha=0.5) +
+  scale_x_continuous(breaks=integer_breaks()) + 
   facet_wrap(~facet_name, scale='free') +
-  theme(legend.position = 'bottom')
+  ylab('BIC') + xlab('degree') + 
+  theme_bw() + 
+  theme(legend.position = 'none')
+dev.off()
