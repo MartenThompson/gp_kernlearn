6726f8029dea92f40557189507f76d18e818c69a
Marten Thompson <martenthompson@gmail.com>
2023-04-05 08:25:14 -0500

    two new norms: max diff b/t obs and sym eigenvalues, max central diff of log(eigenvales)

--- package/gp_kernlearn/code/norms.R
+++ package/gp_kernlearn/code/norms.R
@@ -1,16 +1,43 @@
-weighted_eval_cov_maker <- function(K, schedule) {
-  if (K != length(schedule)) {
-    stop('Weight schedule should be same as K (', K, '); is ', length(schedule))
+weighted_eval_cov_maker <- function(L, schedule) {
+  if (L != length(schedule)) {
+    stop('Weight schedule should be same as L (', L, '); is ', length(schedule))
   }
   
   weighted_eval_cov <- function(mat) {
     cov.mat <- cov(mat) 
-    e.vals <- eigen(cov.mat, symmetric = TRUE, only.values = TRUE)[[1]][1:K]
+    e.vals <- eigen(cov.mat, symmetric = TRUE, only.values = TRUE)[[1]][1:L]
     weighted.mean(e.vals, schedule)
   }
   
   return(weighted_eval_cov)
 }
+
+max_eigendiscrepancy_cov_maker <- function(obs.mat) {
+  e.vals.obs <- eigen(cov(obs.mat), symmetric = TRUE, only.values = TRUE)[[1]]
+  
+  max_eigendiscrepancy_cov <- function(mat) {
+    cov.mat <- cov(mat) 
+    e.vals <- eigen(cov.mat, symmetric = TRUE, only.values = TRUE)[[1]]
+    return(max(abs(e.vals.obs - e.vals)))
+  }
+  return(max_eigendiscrepancy_cov)
+}
+
+central_difference <- function(values) {
+  n <- length(values) 
+  cent.diffs <- rep(NA, n) # leave 1 and n NA for clarity
+  for (i in 2:(n-1)){
+    cent.diffs[i] <- values[i+1] + values[i-1] - 2*values[i]
+  }
+  return(cent.diffs)
+}
+
+max_centraldifflogeigen_cov <- function(mat) {
+  cov.mat <- cov(mat)
+  e.vals <- eigen(cov.mat, symmetric = TRUE, only.values = TRUE)[[1]]
+  return(which.max(central_difference(log(e.vals))))
+}
+
 
 # aka Euclidean norm, sqrt of sum of |a_ij|^2
 frob_norm_cov <- function(mat) {
