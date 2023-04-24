10a390408d1e06f0b60d19af6e31bb8be0b8df66
Marten Thompson <martenthompson@gmail.com>
2023-04-24 13:16:14 -0500

    acv learning of covariance

--- package/gp_kernlearn/code/acv_est.R
+++ package/gp_kernlearn/code/acv_est.R
@@ -15,7 +15,7 @@ stan_beta_post <- function(Y, B.0, n.mcmc) {
 
 
 # Produces n.mcmc estimates of covariance values up to lag (default max possible).
-acv_est_single <- function(B.0, beta.post.samples, lag=(N.x-1)) {
+acv_est_single <- function(B.0, beta.post.samples, lag=(N.x-1), type='covariance') {
   N.x <- dim(B.0)[1]
   
   if (lag >= N.x) {
@@ -24,23 +24,18 @@ acv_est_single <- function(B.0, beta.post.samples, lag=(N.x-1)) {
   
   n.mcmc <- dim(beta.post.samples)[1]
   Y.est <- beta.post.samples %*% t(B.0)
-  acv.out <- acf(t(Y.est), plot=FALSE, type='covariance', lag.max = lag)
-
-  # only keep the cov from within a given beta sample (no cross terms)
-  acv.ests <- matrix(NA, n.mcmc, lag+1)
-  for (i in 1:n.mcmc) {
-    acv.ests[i,] <- acv.out$acf[,i,i]
-  }
   
-  #acv.out GB of likely unneeded info
+  acv.ests <- t(apply(Y.est, 1, function(row) {acf(row, plot=FALSE, type=type, lag.max=lag)$acf}))
+  
+  #full output of acf is GB of likely unneeded info
   return(list(
     acv.ests=acv.ests,
-    acv.all=acv.out$acf
+    Y.est = Y.est
   ))
 }
 
 
-est_sigma_acv <- function(Y.list, B.0, n.mcmc, lag=(N.x-1)) {
+est_acv_within <- function(Y.list, B.0, n.mcmc, lag=(N.x-1), type='covariance') {
   N.x <- dim(B.0)[1]
   R <- length(Y.list)
   
@@ -48,28 +43,86 @@ est_sigma_acv <- function(Y.list, B.0, n.mcmc, lag=(N.x-1)) {
     stop('Lag cannot be greater than or equal to N.x=', N.x, ', was ', lag)
   }
   
-  acv.all <- matrix(NA, n.mcmc, lag+1)
+  acv.all <- matrix(NA, n.mcmc, lag+1) # ends up being n.mcmc*R rows
+  yhat.all <- matrix(NA, n.mcmc, N.x)  # ends up being n.mcmc*R rows
+  
+  # Get within sample (r) variance for all n.mcmc-many predicted y.hats.
+  for (r in 1:R) {
+    cat(r, '/', R, '\n')
+    silence.me <- capture.output(
+      beta.post.single <- stan_beta_post(Y.list[[r]], B.0, n.mcmc)  
+    )
+    
+    acv.single <- acv_est_single(B.0, beta.post.single$beta_post, lag, type)
+    resids <- t(apply(acv.single$Y.est, 1, function(row){row-Y.list[[r]]}))
+    
+    if (1==r) {
+      acv.all[1:n.mcmc,] <- acv.single$acv.ests
+      yhat.all[1:n.mcmc,] <- acv.single$Y.est
+    } else {
+      acv.all <- rbind(acv.all, acv.single$acv.ests)
+      yhat.all <- rbind(yhat.all, acv.single$Y.est)
+    }
+  }
+  
+  # Get between sample variance, having n.mcmc-many groups each with R values.
+  yhat.all.means <- apply(yhat.all, 1, mean)
+  acf.bt.mcmc <- matrix(NA, nrow=n.mcmc, ncol=(lag+1))
+  for (i in 1:n.mcmc) {
+    idx.group <- seq(i, R*n.mcmc, by=n.mcmc)
+    y.means.group <- yhat.all.means[idx.group]
+    acf.bt.mcmc[i,] <- acf(y.means.group, lag.max = lag, type='covariance', plot=FALSE)$acf[,1,1]
+  }
+  
+  # Combine within and between sample variance. 
+  # We have R*n.mcmc 'within' acf vectors (ie acv.all), and we have n.mcmc 'between'
+  # acf vectors (ie acf.bt.mcmc).
+  acv.all.w.bt <- acv.all
+  for (i in 0:(nrow(acv.all)-1)) {
+    acv.all.w.bt[i+1,] <- acv.all.w.bt[i+1,]+acf.bt.mcmc[(i%%n.mcmc)+1]
+  }
+  
+  return(list(
+    acv.all=acv.all,
+    acv.all.w.bt=acv.all.w.bt,
+    yhat.all=yhat.all,
+    resids=resids
+  ))
+  
+}
+
+
+
+est_acv_bt <- function(Y.list, B.0, n.mcmc, lag=(N.x-1), type='covariance') {
+  N.x <- dim(B.0)[1]
+  R <- length(Y.list)
+  
+  if (lag >= N.x) {
+    stop('Lag cannot be greater than or equal to N.x=', N.x, ', was ', lag)
+  }
+  
+  Y.hat.all <- matrix(NA, nrow=n.mcmc, ncol=N.x) # eventually R*N.x cols
   
   for (r in 1:R) {
     cat(r, '/', R, '\n')
     silence.me <- capture.output(
       beta.post.single <- stan_beta_post(Y.list[[r]], B.0, n.mcmc)  
     )
-    acv.single <- acv_est_single(B.0, beta.post.single$beta_post, lag)
     
     if (1==r) {
-      acv.all[1:n.mcmc,] <- acv.single$acv.ests
+      Y.hat.all[1:n.mcmc,1:N.x] <- beta.post.single$beta_post %*% t(B.0)
     } else {
-      acv.all <- rbind(acv.all, acv.single$acv.ests)
+      Y.hat.all <- cbind(Y.hat.all, beta.post.single$beta_post %*% t(B.0))
     }
   }
   
+  acv.ests <- t(apply(Y.hat.all, 1, function(row) {acf(row, plot=FALSE, type=type, lag.max=lag)$acf}))
+  
   return(list(
-    acv.all=acv.all
+    acv.all=acv.ests
   ))
   
 }
-
 
 
 
@@ -89,3 +142,32 @@ make_sigmahat_from_acv <- function(acv.single) {
   
   return(Sigma.hat)
 }
+
+
+
+#(var.t <- var(unlist(Y.train)))
+#var.w <- sum(sapply(1:R, function(r){(1/R)*var(Y.train[[r]])}))
+#var.b <- sum(sapply(1:R, function(r){(1/R)*(mean(Y.train[[r]]) - mean(unlist(Y.train)))^2}))
+#var.w+var.b
+
+
+S_B <- function(Y.all, r, s) {
+  T <- length(unlist(Y.all)) 
+  Y.T.mean <- mean(unlist(Y.all))
+  Y.r.mean <- mean(Y.all[[r]])
+  Y.s.mean <- mean(Y.all[[s]])
+  Y.t <- unlist(c(Y.all[[r]], Y.all[[s]]))
+  ss <- sum(Y.t-Y.T.mean)
+  #(1/T)*
+  return((Y.r.mean-Y.s.mean)*(ss))
+}
+
+#S_B(Y.train, 1,4)
+#lag <- 5
+#sum(sapply((1+lag):R, function(r){S_B(Y.train,r, r-lag)}))
+#sum(sapply((1+lag):R, function(r){S_B(Y.train,r-lag, r)}))
+
+
+
+#Y.ea.mean <- sapply(1:R, function(r){mean(Y.list[[r]])})
+#acf.bt <- acf(Y.ea.mean, lag.max=lag, type=type, plot=FALSE)$acf[,1,1]
--- package/gp_kernlearn/code/acv_est_analysis.R
+++ package/gp_kernlearn/code/acv_est_analysis.R
@@ -17,44 +17,142 @@ gen_data <- function(n.train.datasets, Kern) {
   return(Y.train)
 }
 
+plot_results <- function(acv.est.all, acv.ytrain, acv.true, ylab='cov', title='', alpha=0.05, thin.mod=500) {
+  lag <- ncol(acv.est.all)
+  ymin <- min(quantile(acv.est.all, 0.01), acv.ytrain, acv.true)
+  ymax <- max(quantile(acv.est.all,0.99), acv.ytrain, acv.true)
+  
+  plot(ts(acv.est.all[1,]), col=rgb(0,0,0,0.15), 
+       ylim=c(ymin, ymax), xaxs='i',
+       xlab='lag', ylab=ylab, main=title)
+  for (i in 2:nrow(acv.est.all)) {
+    if (i %% thin.mod == 0) {
+      lines(ts(acv.est.all[i,]), col=rgb(0,0,0,0.15))  
+    }
+  }
+  
+  pd.quant.pointwise <- apply(acv.est.all, 2, function(c) {quantile(c, c(alpha/2, .5, 1-alpha/2))})
+  polygon(c(1:lag, lag:1), c(pd.quant.pointwise[1,], rev(pd.quant.pointwise[3,])), border=NA, col=rgb(1,0,0,0.25))
+  lines(ts(pd.quant.pointwise[2,]), col='red', lwd=2)
+  lines(ts(acv.true), col='green', lwd=2)
+  lines(ts(acv.ytrain), col='blue', lwd=2, lty=2)
+  legend('topright', legend=c('True', 'Median Est', 'Y.train'), col=c('green', 'red', 'blue'), lwd=3, lty=c(1,1,2))
+}
 
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#### Data Gen ####
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 set.seed(212)
-R <- 100 # R = 200 has very good agreement b/t D.1 and Lambda
+R <- 50
 n <- 100
 X <- matrix(seq(-1,1, length.out=n), ncol=1)
-Sigma.true <- rbf_kernel(X, 5, 0.05, 0.1) # cubic_kernel(X, 1,1,1,1,1e-6)
+sig.0 <- 1
+sig.1 <- 0.1
+fuzz <- 1e-1
+Sigma.true <- rbf_kernel(X, sig.0, sig.1, fuzz) 
 Y.train <- gen_data(R, Sigma.true)
 
-J <- 7
+plot(Y.train[[1]], col=rgb(1,1,1), ylim=c(min(unlist(Y.train)), max(unlist(Y.train))))
+for (i in 1:5) {
+  lines(Y.train[[i]])
+}
+
+# Causes of underestimating variance:
+  # Noise: think of a flat line fit (zero var), and obs noisey (some var).
+  # x window
+  # var within and var b/t
+  # J small
+
+# both within and b/t variance
+var(unlist(Y.train))
+# just within
+temp <- lapply(Y.train, function(x) {var(x)})
+summary(unlist(temp))
+
+
+J <- 8
 basis_maker <- make_legendre1D_basis_maker(J-1)
-#basis_maker <- make_monomial1D_basis_maker(J)
 B.0 <- basis_maker(X)
 
-acv.out <- est_sigma_acv(Y.train, B.0, 1e3)
-dim(acv.out$acv.all)
-#image(acv.out$acv.all)
-image(make_sigmahat_from_acv(acv.out$acv.all[3000,]))
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#### Analysis: Within Funct Var Only ####
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+lag <- min(n-1, 25)
+acv.cov.within <- est_acv_within(Y.train, B.0, 1e3, lag, 'covariance')
+#acv.cor.within <- est_acv_within(Y.train, B.0, 1e3, lag, 'correlation')
+
+# looks reasonable
+image(make_sigmahat_from_acv(acv.cov.within$acv.all[1,]))
+
+# tightly correlated components, can probably use element-wise quantiles for HPD
+#plot(acv.cov.within$acv.all[,2], acv.cov.within$acv.all[,3], pch=16)
+
+plot_results(acv.cov.within$acv.all, 
+             acf(unlist(Y.train), lag=lag, plot=FALSE, type='covariance')$acf, 
+             Sigma.true[1,],
+             'Covariance', paste0('RBF(', sig.0,',', sig.1, ',', fuzz, '). Within. J=', J, '. Nx=', n, '. R=', R),
+             )
 
 
-plot(acv.out$acv.all[,2],acv.out$acv.all[,3], pch=16)
+plot_results(acv.cov.within$acv.all.w.bt, 
+             acf(unlist(Y.train), lag=lag, plot=FALSE, type='covariance')$acf, 
+             Sigma.true[1,],
+             'Covariance', paste0('RBF(', sig.0,',', sig.1, ',', fuzz, '). Within. J=', J, '. Nx=', n, '. R=', R),
+)
 
-plot(ts(acv.out$acv.all[1,]), col=rgb(0,0,0,0.15), ylim=c(-5,15))
-for (i in 2:nrow(acv.out$acv.all)) {
-  if (i %% 500 == 0) {
-    lines(ts(acv.out$acv.all[i,]), col=rgb(0,0,0,0.15))  
-  }
+
+
+plot(ts(acf(Y.train[[1]], lag=15, plot=FALSE, type='covariance')$acf), ylim=c(-4,8))
+for (i in 2:length(Y.train)) {
+  lines(ts(acf(Y.train[[i]], lag=15, plot=FALSE, type='covariance')$acf))
 }
-alpha <- 0.05
-pd.quant.pointwise <- apply(acv.out$acv.all, 2, function(c) {quantile(c, c(alpha/2, .5, 1-alpha/2))})
-polygon(c(1:100, 100:1), c(pd.quant.pointwise[1,], rev(pd.quant.pointwise[3,])), border=NA, col=rgb(1,0,0,0.25))
-lines(ts(pd.quant.pointwise[2,]), col='red', lwd=2)
-lines(ts(Sigma.true[1,]), col='green', lwd=2)
 
 
-Y.mat <- matrix(unlist(Y.train), ncol=n, byrow = TRUE)
-Y.acv <- acf(Y.mat, lag.max = 99, type='covariance', plot=FALSE)
-plot(ts(Y.acv$acf[,1,1]))
-for (i in 2:100) {
-  lines(ts(Y.acv$acf[,i,i]))
-}
+
+
+plot_results(temp, 
+             acf(unlist(Y.train), lag=lag, plot=FALSE, type='covariance')$acf, 
+             Sigma.true[1,],
+             'Covariance', paste0('RBF(', sig.0,',', sig.1, ',', fuzz, '). Within. J=', J),
+)
+
+
+plot_results(acv.cor.within$acv.all, 
+             acf(unlist(Y.train), lag=lag, plot=FALSE, type='correlation')$acf, 
+             cov2cor(Sigma.true)[1,],
+             'Correlation', paste0('RBF(', sig.0,',', sig.1, ',', fuzz, '). Within. J=', J))
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#### Analysis: Within & Between Funct Var ####
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+acv.cov.bt <- est_acv_bt(Y.train, B.0, 1e3, lag, 'covariance')
+acv.cor.bt <- est_acv_bt(Y.train, B.0, 1e3, lag, 'correlation')
+
+plot_results(acv.cov.bt$acv.all, 
+             acf(unlist(Y.train), lag=lag, plot=FALSE, type='covariance')$acf, 
+             Sigma.true[1,],
+             'Covariance', paste0('RBF(', sig.0,',', sig.1, ',', fuzz, '). B/t. J=', J))
+
+plot_results(acv.cor.bt$acv.all, 
+             acf(unlist(Y.train), lag=lag, plot=FALSE, type='correlation')$acf, 
+             cov2cor(Sigma.true)[1,],
+             'Correlation', paste0('RBF(', sig.0,',', sig.1, ',', fuzz, '). B/t. J=', J))
+
+###
+
+
+temp.stan <- stan_beta_post(Y.train[[1]], B.0, 1e3)
+
+p <- ncol(B.0)
+df <- n-p
+V.beta <- solve(t(B.0)%*%B.0)
+beta.ols <- solve(t(B.0)%*%B.0)%*%t(B.0)%*%Y.train[[1]]
+s2 <- ((1/(n-p))*t(Y.train[[1]] - B.0%*%beta.ols)%*%(Y.train[[1]] - B.0%*%beta.ols))[1,1]
+t.scale.mat <- s2*(matrix(1, ncol=n, nrow=n) + B.0%*%V.beta%*%t(B.0))
+t.pp.var <- (df/(df-2))*t.scale.mat
+
 
